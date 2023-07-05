91fa2f42d214929bc29a097da8650645c44a9ef4
Andrew Njoo <andrewnjoo@Andrews-MacBook-Pro.local>
2023-07-05 14:21:24 -0700

    use bootstrap, add review

---
+++ 23-07-05-the-beach.md
@@ -0,0 +1,17 @@
+Review: "The Beach" by Alex Garland
+
+Rating: ★★★★★ (4/5)
+
+Review Date: July 5, 2023
+
+In this blog post I will review the audiobook version of The Beach by Alex Garland, which is available on Youtube. This could be a great alternative or companion to the novel. For example, auditory learners might benefit.
+
+---
+
+This is a great story by Alex Garland; the prose is easy to follow. The characters are likable, and it's fun piggy backing on the adventures of the main trio: Richard, Etienne, and Francois.
+
+The audiobook itself is well narrated. The narrator in this particular audiobook even does the accents for various characters like Etienne, and Bugs.
+
+The length is around 6 and a half hours. The print edition is 371 pages.
+
+Overall, I would recommend this audiobook to anyone interested in going on adventures. The best part is that the story is available in three mediums including film, audiobook and prose.
---
+++ script.js
@@ -0,0 +1,37 @@
+const extractDateFromFileName = (fileName) => fileName.slice(0, 8);
+const toTitleCase = (str) => {
+  return str.replace(
+    /\w\S*/g,
+    function(txt) {
+      return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    }
+  );
+}
+let fileNames = [
+    "23-06-24-simple-path.md",
+    "23-07-05-the-beach.md"
+];
+let reviewSelect = document.getElementById("reviewSelect");
+let reviewContainer = document.getElementById("review");
+
+fileNames.forEach(function(fileName) {
+    let filePath = './' + fileName;
+    let reviewDate = extractDateFromFileName(fileName);
+    let option = document.createElement("option");
+    option.value = filePath;
+    option.text = toTitleCase(reviewDate + " - " + fileName.slice(9, -3).replace(/-/g, " "));
+    reviewSelect.appendChild(option);
+});
+
+function showReview() {
+    let filePath = reviewSelect.value;
+
+    if (filePath === "") {
+        reviewContainer.style.display = "none";
+    } else {
+        let selectedOption = reviewSelect.options[reviewSelect.selectedIndex];
+        let reviewDate = selectedOption.text.split(" - ")[1];
+        reviewContainer.innerHTML = '<h3>' + reviewDate + '</h3><zero-md src="' + filePath + '"></zero-md>';
+        reviewContainer.style.display = "block";
+    }
+}
