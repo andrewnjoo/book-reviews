f49776c9239fc4e0e66d9961f64608b93820ed38
Andrew Njoo <adnjoo@gmail.com>
2023-07-10 15:58:39 -0700

    add alice review

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
-const extractDateFromFileName = fileName => fileName.slice(0, 8);
-const fileNames = ["23-06-24-the-simple-path-to-wealth.md", "23-07-05-the-beach.md"];
+const extractDateFromFileName = (fileName) => fileName.slice(0, 8);
+const fileNames = [
+  "23-06-24-the-simple-path-to-wealth.md",
+  // "23-07-05-the-beach.md",
+  "23-07-10-alice's-adventures-in-wonderland.md",
+];
 const reviewSelect = document.getElementById("reviewSelect");
 const reviewContainer = document.getElementById("review");
 
-fileNames.forEach(fileName => {
+fileNames.forEach((fileName) => {
   const filePath = `./reviews/${fileName}`;
   const reviewDate = extractDateFromFileName(fileName);
   const option = document.createElement("option");
   option.value = filePath;
-  option.text = `${reviewDate} - ${(fileName.slice(9, -3).replace(/-/g, " ")).toTitleCase()}`
+  option.text = `${reviewDate} - ${fileName
+    .slice(9, -3)
+    .replace(/-/g, " ")
+    .toTitleCase()}`;
   reviewSelect.appendChild(option);
 });
 
