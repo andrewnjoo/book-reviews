b2ebd36f8610c7ce6c18ca59d9998169069418ce
Andrew Njoo <adnjoo@gmail.com>
2023-07-13 20:36:47 -0700

    sort chronologically

--- script.py
+++ script.py
@@ -13,21 +13,24 @@ section = soup.find("section")
 
 section.clear()
 
+# Sort the file names chronologically, with the newest first
+file_names.sort(reverse=True)
+
 for file_name in file_names:
     review_path = os.path.join(reviews_dir, file_name)
     review_title = ""
-    
+
     with open(review_path) as review_file:
         first_line = review_file.readline().strip()
         if first_line.startswith("Review:"):
             review_title = first_line[8:].strip()
-    
+
     div_tag = soup.new_tag("div")
     # Remove the ".md" extension from the file_name
     file_name_without_extension = os.path.splitext(file_name)[0]
     a_tag = soup.new_tag("a", href=f"./reviews/{file_name_without_extension}")
     a_tag.string = f"{file_name[:8]} Review of {review_title}"
-    
+
     div_tag.append(a_tag)
     section.append(div_tag)
 
